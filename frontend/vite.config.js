296c8e217bb75ad37ea6c92b873839a07193631c
xKhanDev <safeerkhanxofficial@gmail.com>
2024-12-22 20:58:55 +0500

    CHANGE PROXY

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   server: {
     port:3000,
     proxy: {
-      "/dashboared/": "http://localhost:5000",
-      "/form/": "http://localhost:5000",
+      "/dashboared/": "https://safeerxkhan.vercel.app/",
+      "/form/": "https://safeerxkhan.vercel.app/",
     }
   },
 })
